547ff7c8b2cb91497b75340303304e5407186e59
gtsaki99 <gtsak1999@hotmail.com>
2022-05-15 23:22:21 +0300

    Added another sorting button and functionality to all sorting buttons.

--- app/src/main/java/com/example/restock/OrderHistory.java
+++ app/src/main/java/com/example/restock/OrderHistory.java
@@ -14,12 +14,17 @@ import com.example.restock.RecycleView.OrdersRecyclerAdapter;
 import com.example.restock.objects.Order;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class OrderHistory extends AppCompatActivity {
     RecyclerView ordersRecyclerView;
     LinearLayoutManager linearLayoutManager;
-    RecyclerView.Adapter<OrdersRecyclerAdapter.ViewHolder> ordersAdapter;
+    RecyclerView.Adapter<OrdersRecyclerAdapter.ViewHolder> ordersAdapterDef, ordersAdapterNoDesc, ordersAdapterTotAsc, ordersAdapterTotDesc;
     Order orders[];
-    FloatingActionButton totalAsc, totalDesc, orderNoDesc, sortBy;
+    FloatingActionButton totalAsc, totalDesc, orderNoAsc, orderNoDesc, sortBy;
     Animation fabOpen, fabClose, rotateForward, rotateBackward;
 
     boolean isOpen = false;
@@ -38,11 +43,12 @@ public class OrderHistory extends AppCompatActivity {
 
         orders = dbHandler.getAllOrders();
 
-        ordersAdapter = new OrdersRecyclerAdapter(orders, this);
-        ordersRecyclerView.setAdapter(ordersAdapter);
+        ordersAdapterDef = new OrdersRecyclerAdapter(orders, this);
+        ordersRecyclerView.setAdapter(ordersAdapterDef);
 
         totalAsc = (FloatingActionButton) findViewById(R.id.totalAscendingFloatingBtn);
         totalDesc = (FloatingActionButton) findViewById(R.id.totalDescendingFloatingBtn);
+        orderNoAsc = (FloatingActionButton) findViewById(R.id.orderNoAscendingBtn);
         orderNoDesc = (FloatingActionButton) findViewById(R.id.orderNoDescendingBtn);
         sortBy = (FloatingActionButton) findViewById(R.id.sortingFloatingBtn);
 
@@ -62,36 +68,138 @@ public class OrderHistory extends AppCompatActivity {
         totalAsc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(OrderHistory.this, "totalAscending clicked", Toast.LENGTH_SHORT).show();
+                Order[] ordersTemp = orders;
+                Arrays.sort(ordersTemp, new Comparator<Order>() {
+                    @Override
+                    public int compare(Order o1, Order o2) {
+                        if(o1.getTotalPrice() > o2.getTotalPrice())
+                            return 1;
+                        else if(o2.getTotalPrice() > o1.getTotalPrice())
+                            return -1;
+                        else
+                            return 0;
+                    }
+                });
+                ordersAdapterTotAsc = new OrdersRecyclerAdapter(ordersTemp, OrderHistory.this);
+                ordersRecyclerView.swapAdapter(ordersAdapterTotAsc, false);
             }
         });
 
         totalDesc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(OrderHistory.this, "totalDescending clicked", Toast.LENGTH_SHORT).show();
+                Order[] ordersTemp = orders;
+                Arrays.sort(ordersTemp, new Comparator<Order>() {
+                    @Override
+                    public int compare(Order o1, Order o2) {
+                        if(o1.getTotalPrice() > o2.getTotalPrice())
+                            return -1;
+                        else if(o2.getTotalPrice() > o1.getTotalPrice())
+                            return 1;
+                        else
+                            return 0;
+                    }
+                });
+                ordersAdapterTotDesc = new OrdersRecyclerAdapter(ordersTemp, OrderHistory.this);
+                ordersRecyclerView.swapAdapter(ordersAdapterTotDesc, false);
+            }
+        });
+
+        orderNoAsc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Arrays.sort(orders, new Comparator<Order>() {
+                    @Override
+                    public int compare(Order o1, Order o2) {
+                        if(o1.getOrderNumber() > o2.getOrderNumber())
+                            return 1;
+                        else if(o2.getOrderNumber() > o1.getOrderNumber())
+                            return -1;
+                        else
+                            return 0;
+                    }
+                });
+                ordersAdapterDef = new OrdersRecyclerAdapter(orders, OrderHistory.this);
+                ordersRecyclerView.swapAdapter(ordersAdapterDef, false);
             }
         });
 
         orderNoDesc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(OrderHistory.this, "orderNoDescending clicked", Toast.LENGTH_SHORT).show();
+                Order[] ordersTemp = orders;
+                Arrays.sort(ordersTemp, new Comparator<Order>() {
+                    @Override
+                    public int compare(Order o1, Order o2) {
+                        if(o1.getOrderNumber() > o2.getOrderNumber())
+                            return -1;
+                        else if(o2.getOrderNumber() > o1.getOrderNumber())
+                            return 1;
+                        else
+                            return 0;
+                    }
+                });
+                ordersAdapterNoDesc = new OrdersRecyclerAdapter(ordersTemp, OrderHistory.this);
+                ordersRecyclerView.swapAdapter(ordersAdapterNoDesc, false);
+            }
+        });
+
+        sortBy.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(OrderHistory.this, "Sort by:", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        totalAsc.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(OrderHistory.this, "Total price ascending", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        totalDesc.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(OrderHistory.this, "Total price descending", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        orderNoAsc.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(OrderHistory.this, "Order no ascending", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        orderNoDesc.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(OrderHistory.this, "Order ascending", Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
     }
+
 
     private void animateFab() {
         if(isOpen) {
             sortBy.startAnimation(rotateBackward);
             totalAsc.startAnimation(fabClose);
             totalDesc.startAnimation(fabClose);
+            orderNoAsc.startAnimation(fabClose);
             orderNoDesc.startAnimation(fabClose);
             totalAsc.setVisibility(View.INVISIBLE);
             totalDesc.setVisibility(View.INVISIBLE);
+            orderNoAsc.setVisibility(View.INVISIBLE);
             orderNoDesc.setVisibility(View.INVISIBLE);
             totalDesc.setClickable(false);
             totalAsc.setClickable(false);
+            orderNoAsc.setClickable(false);
             orderNoDesc.setClickable(false);
             isOpen = false;
         }
@@ -99,14 +207,18 @@ public class OrderHistory extends AppCompatActivity {
             sortBy.startAnimation(rotateForward);
             totalAsc.startAnimation(fabOpen);
             totalDesc.startAnimation(fabOpen);
+            orderNoAsc.startAnimation(fabOpen);
             orderNoDesc.startAnimation(fabOpen);
             totalAsc.setVisibility(View.VISIBLE);
             totalDesc.setVisibility(View.VISIBLE);
+            orderNoAsc.setVisibility(View.VISIBLE);
             orderNoDesc.setVisibility(View.VISIBLE);
             totalDesc.setClickable(true);
             totalAsc.setClickable(true);
+            orderNoAsc.setClickable(true);
             orderNoDesc.setClickable(true);
             isOpen = true;
         }
     }
+
 }
